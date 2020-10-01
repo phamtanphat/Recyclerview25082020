1cf07e19e13422366c0e45336d9827d83a5448be
phatdroid94 <phatdroid94@gmail.com>
2020-10-01 20:42:12 +0700

    adapter

--- app/src/main/java/com/example/recyclerview25082020/MainActivity.java
+++ app/src/main/java/com/example/recyclerview25082020/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.recyclerview25082020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +13,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // 1 : Add thu vien recyclerview
     }
+
 }
---
+++ app/src/main/java/com/example/recyclerview25082020/MovieAdapter.java
@@ -0,0 +1,55 @@
+package com.example.recyclerview25082020;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+//generic type
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
+
+    List<Movie> mArrayMovie;
+
+    public MovieAdapter(List<Movie> mArrayMovie) {
+        this.mArrayMovie = mArrayMovie;
+    }
+
+    @NonNull
+    @Override
+    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.layout_item_movie,parent,false);
+        return new MovieViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+        Movie movie = mArrayMovie.get(position);
+        holder.imgMovie.setImageResource(movie.getImage());
+        holder.tvName.setText(movie.getName());
+        holder.tvQualify.setText(movie.getQualify());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mArrayMovie == null ? 0 : mArrayMovie.size();
+    }
+
+    class MovieViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView imgMovie;
+        TextView tvName,tvQualify;
+        public MovieViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imgMovie = itemView.findViewById(R.id.imageView);
+            tvName = itemView.findViewById(R.id.textviewName);
+            tvQualify = itemView.findViewById(R.id.textviewqualify);
+        }
+    }
+}
