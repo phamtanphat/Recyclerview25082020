0f5a6d6987d5d23357acee08a2edcebc76e87a5b
phatdroid94 <phatdroid94@gmail.com>
2020-10-01 21:03:17 +0700

    fix ui

--- app/src/main/java/com/example/recyclerview25082020/MainActivity.java
+++ app/src/main/java/com/example/recyclerview25082020/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.recyclerview25082020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -22,9 +23,17 @@ public class MainActivity extends AppCompatActivity {
         mRcvMovie = findViewById(R.id.recyclerview);
 
         // Khai bao data gia cho mang
-//        mArrayMovie = new ArrayList<>();
-//        mArrayMovie.add(new Movie("RÒM",R.drawable.phim_rom,"HD"));
-        mArrayMovie = Movie.getDataMock();
+        mArrayMovie = new ArrayList<>();
+        mArrayMovie.add(new Movie("RÒM",R.drawable.phim_rom,"HD"));
+        mArrayMovie.add(new Movie("GIA ĐÌNH CHÂN TO PHIÊU LƯU KÝ",R.drawable.phim_chan_to_phieu_luu_ky,"CAM"));
+        mArrayMovie.add(new Movie("DINH THỰ XÁC SỐNG",R.drawable.phim_dinh_thu_xac_song,"HD"));
+        mArrayMovie.add(new Movie("MƯỢN XÁC",R.drawable.phim_muon_xac,"HD"));
+        mArrayMovie.add(new Movie("PHÒNG THỦ ĐỊA CẦU",R.drawable.phim_phong_thu_dia_cau,"HD"));
+
+        mMovieAdapter = new MovieAdapter(mArrayMovie);
+
+        mRcvMovie.setLayoutManager(new GridLayoutManager(this,2));
+        mRcvMovie.setAdapter(mMovieAdapter);
     }
 
 }
